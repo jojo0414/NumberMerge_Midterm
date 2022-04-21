31e2fcc5ed378a8d383bd4c2ba6c35f9cbd8ea0b
jojo <jojo22123770@gmail.com>
2022-04-21 23:16:11 +0800

    Initial Commit

---
+++ NumberMerge_Midterm/NumberMerge_MidtermApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberMerge_MidtermApp.swift
+//  NumberMerge_Midterm
+//
+//  Created by 陳昕喬 on 2022/4/21.
+//
+
+import SwiftUI
+
+@main
+struct NumberMerge_MidtermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
